92e469dfa54d256ac428ea6f6b0404b2abb32322
Eshwar Paygude <140734986+Eshwar154@users.noreply.github.com>
2025-12-30 04:14:07 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs,not browser
+
+console.log(3 + 3)
+
+console.log("eshwar")
+
+let name = "eshwar"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => for large value
+// string => ""
+// boolean => true/false
+// null =>  standalone value
+// undefined => value is not assigned 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); //object
