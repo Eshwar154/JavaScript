085be0c1cb1a8ec2efe7bec927ad03b0fd31150c
Eshwar Paygude <140734986+Eshwar154@users.noreply.github.com>
2026-01-01 05:16:10 +0000

    objects in Js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+// singleton  making object by constructor is called singleton
+
+//Object.create
+
+// object literals
+
+//symbol declaration
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "hitesh",   //here name key is also a string in object
+    "full name":"Hitesh Choudhary", // now we cannot access this value  as .full name by anychance.
+    [mySym]:"mykey1",  // [mySym] this syntax makes it symbol in object
+    age:18,
+    location:"Jaipur",
+    email:"hitesh@google.com",
+    isLoggedIn:false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+// console.log(typeof(JsUser[mySym]))
+
+JsUser.email = "hitesh@chatgpt.com"
+//Object.freeze(JsUser)                     // lock the object
+JsUser.email = "hitesh@microsoft.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`hello JS user,${this.name}`);
+}
+
+console.log(JsUser.greeting()) 
+console.log(JsUser.greetingTwo()) 
