2d1f500fd7b2e07c13ce636a92d8f6f51faa3bd9
Eshwar154 <eshwarpaygude@gmail.com>
2026-01-03 13:09:52 +0530

    control statement in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,65 @@
+// if statement
+const isUserLoggedIn = true
+const temperature = 41
+
+
+// if(temperature < 50){
+//      console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// //comparsion operator
+// < less than
+// >  greater than
+// <= less than or equal
+// >= gtreater than or equal
+// = assignment operator
+// == checking equal
+// != not equal
+// === is also check value &  data type
+//!== checks value and data type
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power:${power}`);
+// }
+
+//  console.log(`User power: ${power}`);
+
+//const balance = 1000
+
+//if(balance > 500) console.log("test"), console.log("test2"); // do not write this type code // implict scopeconsole.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+//logical operator
+
+if (userLoggedIn && debitCard && 2==3){
+    console.log("allow to buy course")
+}
+
+if (loggedInFromGoogle || loggedInFromEmail ){
+    console.log("user logged in");
+}
+
+
+
